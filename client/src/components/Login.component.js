f3bf2e382e342a2d7dbe0337685224a06343139b
Ryan Mount <rmount96@outlook.com>
2021-02-03 11:32:22 -0600

    created new react app and test client

---
+++ client/src/components/Login.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h3>React Login Component</h3>
+            </div>
+        );
+    }
+}
